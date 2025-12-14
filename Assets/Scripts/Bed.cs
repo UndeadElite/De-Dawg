bb6094e48c2838e5c9f25ffb22b6d3390ce3b5b1
UndeadElite <soldiergamer3171@gmail.com>
2025-12-14 17:02:40 +0100

    SleepCutscene start
    made a few cinemachine cameras for timeline to use, made a fade from black and then now needs to make the whole sleep cutscene with the dog appearing in the kitchen door

--- Assets/Scripts/Bed.cs
+++ Assets/Scripts/Bed.cs
@@ -4,7 +4,6 @@ using UnityEngine.SceneManagement;
 public class Bed : MonoBehaviour, IInteractable
 {
     [SerializeField] GiveFood giveFood;
-
     public void Interact()
     {
         if (giveFood.HaveIGivenFood)
